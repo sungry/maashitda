65eaab11b4be2c3fb476d902ea472b2166e2af14
Sung Bin Choi <sbchoi86@gmail.com>
2015-08-03 15:22:00 -0700

    Add styling and search form using Angular

---
+++ app/filters/findInKeyword.filter.js
@@ -0,0 +1,13 @@
+(function() {
+  'use strict'
+
+  angular.module('maashitda').
+  filter('searchText', function() {
+    return function()
+  })
+
+
+
+
+
+})();
--- app/services/maashitda.service.js
+++ app/services/maashitda.service.js
@@ -1,44 +1,44 @@
-// IFFE here! since functions creates scope in js, this is to prevent polluting global scope.
 (function() {
-  'use strict'
+  'use strict';
 
   angular.module('maashitda').
-    controller('MaashitdaController',[
-      '$scope',
-      function($scope) {
-        $scope.test = 'Hello world!';
-        $scope.restaurants = [
+    factory('MaashitdaService', MaashitdaService);
+
+    function MaashitdaService($q) {
+      var service = {
+        getMockRestaurants: function() {
+          var restaurants = [
           {
             name: '15 Romolo',
             cuisine: 'Contemporary American',
-            neighborhood: 'North Beach',
+            neighborhood: 'North Beach'
             price: '$$',
             keywords: ['vibrant', 'brunch', 'food', 'bar', 'cheap', 'foodie', 'cocktails', 'burger']
           },
-
           {
             name: '1760',
             cuisine: 'Californian',
-            neighborhood: 'Nob Hill',
+            neighborhood: 'Nob Hill'
             price: '$$$',
             keywords: ['vibrant', 'dinner', 'small plates', 'italian', 'sophisticated', 'foodie', 'delicious', 'cocktails']
-
           },
           {
             name: 'Wise Sons Deli',
             cuisine: 'Delicatessen',
-            neighborhood: 'Mission',
+            neighborhood: 'Mission'
             price: '$$',
             keywords: ['vibrant', 'deli', 'food', 'kosher', 'casual','foodie', 'sandwich', 'reuben', 'jewish', 'corned beef', 'local' ]
           },
           {
             name: 'Cotogna',
             cuisine: 'Italian',
-            neighborhood: 'Finance District',
+            neighborhood: 'Finance District'
             price: '$$$',
             keywords: ['vibrant', 'brunch', 'dinner', 'italian', 'food', 'bar', 'foodie', 'obama', 'wine']
           }
           ];
-      }])
+        }
+      };
+      return service;
+    }
 })();
-
