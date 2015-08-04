22b4331fbe33fbc9a35349fddc693aea83a3bb89
Sung Bin Choi <sbchoi86@gmail.com>
2015-08-04 11:29:00 -0700

    Add paths

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
+var path = require("path");
+
+app.use(express.static(path.join(__dirname,'public')));
+
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 var server = app.listen(3000, function() {
