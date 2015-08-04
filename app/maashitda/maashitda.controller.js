0c3e063b28193df39435c383a4997eb87ee69bf2
Sung Bin Choi <sbchoi86@gmail.com>
2015-08-03 17:48:02 -0700

    Rollllllllllllllllllllll

--- app/maashitda/maashitda.controller.js
+++ app/maashitda/maashitda.controller.js
@@ -2,43 +2,19 @@
 (function() {
   'use strict'
 
-  angular.module('maashitda').
-    controller('MaashitdaController',[
-      '$scope',
-      function($scope) {
-        $scope.test = 'Hello world!';
-        $scope.restaurants = [
-          {
-            name: '15 Romolo',
-            cuisine: 'Contemporary American',
-            neighborhood: 'North Beach',
-            price: '$$',
-            keywords: ['vibrant', 'brunch', 'food', 'bar', 'cheap', 'foodie', 'cocktails', 'burger']
-          },
+    angular.module('maashitda').
+  controller('MaashitdaController', MaashitdaController)
 
-          {
-            name: '1760',
-            cuisine: 'Californian',
-            neighborhood: 'Nob Hill',
-            price: '$$$',
-            keywords: ['vibrant', 'dinner', 'small plates', 'italian', 'sophisticated', 'foodie', 'delicious', 'cocktails']
+  function MaashitdaController( $scope, MaashitdaService) {
+    $scope.restaurants = MaashitdaService.getMockRestaurants()
+  }
 
-          },
-          {
-            name: 'Wise Sons Deli',
-            cuisine: 'Delicatessen',
-            neighborhood: 'Mission',
-            price: '$$',
-            keywords: ['vibrant', 'deli', 'food', 'kosher', 'casual','foodie', 'sandwich', 'reuben', 'jewish', 'corned beef', 'local' ]
-          },
-          {
-            name: 'Cotogna',
-            cuisine: 'Italian',
-            neighborhood: 'Finance District',
-            price: '$$$',
-            keywords: ['vibrant', 'brunch', 'dinner', 'italian', 'food', 'bar', 'foodie', 'obama', 'wine']
-          }
-          ];
-      }])
 })();
 
+
+    // angular.module('maashitda').
+  //   controller('MaashitdaController',[
+  //     '$scope',
+  //     function($scope, MaashitdaService) {
+  //       MaashitdaService.getMockRestaurants()
+  //     }])
--- app/services/maashitda.service.js
+++ app/services/maashitda.service.js
@@ -4,37 +4,37 @@
   angular.module('maashitda').
     factory('MaashitdaService', MaashitdaService);
 
-    function MaashitdaService($q) {
+    function MaashitdaService() {
       var service = {
         getMockRestaurants: function() {
           var restaurants = [
           {
-            name: '15 Romolo',
-            cuisine: 'Contemporary American',
-            neighborhood: 'North Beach'
-            price: '$$',
-            keywords: ['vibrant', 'brunch', 'food', 'bar', 'cheap', 'foodie', 'cocktails', 'burger']
+            Name: '15 Romolo',
+            Cuisine: 'Contemporary American',
+            Neighborhood: 'North Beach'
+            Price: '$$',
+            Keywords: ['vibrant', 'brunch', 'food', 'bar', 'cheap', 'foodie', 'cocktails', 'burger']
           },
           {
-            name: '1760',
-            cuisine: 'Californian',
-            neighborhood: 'Nob Hill'
-            price: '$$$',
-            keywords: ['vibrant', 'dinner', 'small plates', 'italian', 'sophisticated', 'foodie', 'delicious', 'cocktails']
+            Name: '1760',
+            Cuisine: 'Californian',
+            Neighborhood: 'Nob Hill'
+            Price: '$$$',
+            Keywords: ['vibrant', 'dinner', 'small plates', 'italian', 'sophisticated', 'foodie', 'delicious', 'cocktails']
           },
           {
-            name: 'Wise Sons Deli',
-            cuisine: 'Delicatessen',
-            neighborhood: 'Mission'
-            price: '$$',
-            keywords: ['vibrant', 'deli', 'food', 'kosher', 'casual','foodie', 'sandwich', 'reuben', 'jewish', 'corned beef', 'local' ]
+            Name: 'Wise Sons Deli',
+            Cuisine: 'Delicatessen',
+            Neighborhood: 'Mission'
+            Price: '$$',
+            Keywords: ['vibrant', 'deli', 'food', 'kosher', 'casual','foodie', 'sandwich', 'reuben', 'jewish', 'corned beef', 'local' ]
           },
           {
-            name: 'Cotogna',
-            cuisine: 'Italian',
-            neighborhood: 'Finance District'
-            price: '$$$',
-            keywords: ['vibrant', 'brunch', 'dinner', 'italian', 'food', 'bar', 'foodie', 'obama', 'wine']
+            Name: 'Cotogna',
+            Cuisine: 'Italian',
+            Neighborhood: 'Finance District'
+            Price: '$$$',
+            Keywords: ['vibrant', 'brunch', 'dinner', 'italian', 'food', 'bar', 'foodie', 'obama', 'wine']
           }
           ];
         }
